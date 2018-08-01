61d68ea460a8618386bf9fece57bcae2775ae5e3
saraqiel <13724275+saraqiel@users.noreply.github.com>
2018-08-01 19:59:15 -0400

    got it!

---
+++ Assets/my_assets/scripts/PlatformBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBehaviour : MonoBehaviour {
+
+    public float Speed = 1.0f, start, end;//frames/second
+	// Use this for initialization
+	void Start () {
+        start = 0;
+        end = 180;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Oscillate();
+    }
+
+    private void Oscillate() {
+        if (start <= end)
+        {
+            start += Time.deltaTime;
+            this.transform.Translate(new Vector3(0, Speed * Time.deltaTime, 0));
+        }
+        else {
+            start = 0;
+            Speed *= -1;
+        }
+    }
+}
