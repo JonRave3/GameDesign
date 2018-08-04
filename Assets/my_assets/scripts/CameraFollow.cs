c0d52dc846c94122f638e12cb83dc3cef7066050
saraqiel <13724275+saraqiel@users.noreply.github.com>
2018-08-04 06:23:40 -0400

    updated fireball
    update fileball, firecannon, and camera following

--- Assets/my_assets/scripts/CameraFollow.cs
+++ Assets/my_assets/scripts/CameraFollow.cs
@@ -4,12 +4,39 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player = null;
-    public Transform target = null;
-	// Use this for initialization
+    Camera pCamera;
+    Transform focus;
+    Vector3 ptr;
+   public float multp;
+    private float rotY;
+    private float rotX = 0.0f;
+    public float clampAngle = 80.0f;
+    public float mouseSensitivity = 100.0f;
+    // Use this for initialization
+    void Start ()
+    {
+        rotY= 0.0f; // rotation around the up/y axis
+    
+		pCamera = GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
 
-    void LateUpdate() {
-        this.transform.LookAt(player.position);
-        this.transform.position = target.position;
+
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+
+
     }
 }
--- Assets/my_assets/scripts/fireballOsc.cs
+++ Assets/my_assets/scripts/fireballOsc.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class fireballOsc : MonoBehaviour {
 
-
-    float move;
-    public float height, speed;
+    Rigidbody rb;
+    public float height, move;
     bool up;
     Vector3 startPos;
     Vector3 goal;
@@ -14,8 +13,9 @@ public class fireballOsc : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
+        rb = GetComponent<Rigidbody>();
         height = 10.0f;
-        move = 9.8f;
+      //  move = 9.8f;
         up = true;
         startPos = transform.position;
         goal = startPos;
@@ -25,30 +25,38 @@ public class fireballOsc : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        goal.y = startPos.y + height;
+        //goal.y = startPos.y + height;
 
+        //if (up)//move up
+        //{
 
-        if (up)//move up
+        //    move *= 1.1f;
+        //    transform.position = Vector3.MoveTowards(transform.position, goal, move * Time.deltaTime);
+        //    if (transform.position == goal)
+        //    {
+        //        move = 0f;
+        //        up = !up;
+        //    }
+        //}
+        //else
+        //{
+        //    move += 2;
+
+        //    transform.position = Vector3.MoveTowards(transform.position, startPos, move * Time.deltaTime);
+        //    if (transform.position == startPos)
+        //    {
+        //        move = 9.8f;
+        //        up = !up;
+        //    }
+        //}
+
+    }
+    private void FixedUpdate()
+    {
+        if (transform.position.y <=startPos.y)
         {
-
-            move *= 1.1f;
-            transform.position = Vector3.MoveTowards(transform.position, goal, move * Time.deltaTime);
-            if (transform.position == goal)
-            {
-                move = 0f;
-                up = !up;
-            }
+            rb.AddForce(transform.up * move);
         }
-        else
-        {
-            move += 2;
 
-            transform.position = Vector3.MoveTowards(transform.position, startPos, move * Time.deltaTime);
-            if (transform.position == startPos)
-            {
-                move = 9.8f;
-                up = !up;
-            }
-        }
     }
 }
